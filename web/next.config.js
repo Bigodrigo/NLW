2feaf8cf7818727d642314f75100d6fd60959cb8
Rodrigo Estabel Boquer <rodrigo-boquer@IPESAUDE.INTRA.RS.GOV.BR>
2023-05-25 11:30:00 -0300

    Ajuste Web e Uploads!

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', '10.52.4.196'],
   },
 }
 
